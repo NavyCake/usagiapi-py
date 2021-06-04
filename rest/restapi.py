1c6028db457f1659186a82a5d762f47f0b1b1c6a
Arthur Phelipe <arthurphelipe112@gmail.com>
2021-06-04 14:06:26 -0300

    Update restapi.py

--- rest/restapi.py
+++ rest/restapi.py
@@ -2,5 +2,5 @@
 import requests
 
 def restAPI(endpoint):
-    http = requests.get("https://usagiapi.danielagc.repl.co" + endpoint)
+    http = requests.get("https://usagiapi.danielagc.repl.co/api/" + endpoint)
     return http.json()["url"]
